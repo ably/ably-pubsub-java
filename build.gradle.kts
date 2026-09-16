ably/ably-java

Add lockstep release pre-flight and a release dry-run CI job
PDR-091b requires core, core-android, device and server to release
together on one version, with partial release impossible:

- A verifyReleaseArtifacts task asserts the exact published artifact
  set, group and lockstep VERSION_NAME (catching module-local
  overrides), failing before anything is uploaded. The release workflow
  runs it ahead of publishAndReleaseToMavenCentral.
- A release-dry-run job in check.yml runs the pre-flight plus
  publishToMavenLocal on every PR, so coordinate and version
  regressions surface continuously rather than on release day.
- README gains a note describing the in-development 2.0 artifact set
  and that 1.x installation instructions still apply.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

build.gradle.kts
@@ -30,6 +30,56 @@ subprojects {
     }
 }
 
+/*
+ * Release pre-flight: the split ships core, core-android, device and server in lockstep
+ * (one version, one run — PDR-091b), so the set of published artifacts and their
+ * coordinates are asserted here and the release workflow fails before anything is
+ * uploaded if they drift. If you add or remove a published module, update this list
+ * deliberately.
+ */
+val expectedReleaseArtifacts = sortedSetOf(
+    "io.ably.pubsub:core:jar",
+    "io.ably.pubsub:core-android:aar",
+    "io.ably.pubsub:device:aar",
+    "io.ably.pubsub:server:jar",
+    "io.ably.pubsub:liveobjects:jar",
+    "io.ably.pubsub:pubsub-adapter:jar",
+    "io.ably.pubsub:network-client-core:jar",
+    "io.ably.pubsub:network-client-default:jar",
+    "io.ably.pubsub:network-client-okhttp:jar",
+)
+
+tasks.register("verifyReleaseArtifacts") {
+    description = "Asserts the published artifact set, group and lockstep version before a release."
+    doLast {
+        val rootVersion = project.property("VERSION_NAME") as String
+        val actual = sortedSetOf<String>()
+        subprojects.filter { it.pluginManager.hasPlugin("com.vanniktech.maven.publish") }.forEach { p ->
+            val artifactId = p.findProperty("POM_ARTIFACT_ID")
+                ?: error("${p.path} applies maven-publish but has no POM_ARTIFACT_ID")
+            val packaging = p.findProperty("POM_PACKAGING") ?: "jar"
+            // The version each module publishes at comes from its effective VERSION_NAME
+            // (a module-local gradle.properties can override the root's — exactly the
+            // lockstep drift this guards against).
+            val moduleVersion = p.findProperty("VERSION_NAME")
+            if (moduleVersion != rootVersion) {
+                error("Lockstep violation: ${p.path} has VERSION_NAME $moduleVersion, expected $rootVersion")
+            }
+            val group = p.findProperty("GROUP")
+            actual.add("$group:$artifactId:$packaging")
+        }
+        if (actual != expectedReleaseArtifacts) {
+            error(
+                "Published artifact set does not match the expected release set.\n" +
+                    "  expected: $expectedReleaseArtifacts\n" +
+                    "  actual:   $actual\n" +
+                    "If this change is deliberate, update expectedReleaseArtifacts in build.gradle.kts."
+            )
+        }
+        logger.lifecycle("Release pre-flight OK: ${actual.size} artifacts at $rootVersion: $actual")
+    }
+}
+
 configure(subprojects) {
     pluginManager.withPlugin("com.vanniktech.maven.publish") {
         extensions.configure<MavenPublishBaseExtension> {

ably/ably-pubsub-java

build.gradle.kts
@@ -31,6 +31,56 @@ subprojects {
     }
 }
 
+/*
+ * Release pre-flight: the split ships core, core-android, device and server in lockstep
+ * (one version, one run), so the set of published artifacts and their
+ * coordinates are asserted here and the release workflow fails before anything is
+ * uploaded if they drift. If you add or remove a published module, update this list
+ * deliberately.
+ */
+val expectedReleaseArtifacts = sortedSetOf(
+    "io.ably.pubsub:core:jar",
+    "io.ably.pubsub:core-android:aar",
+    "io.ably.pubsub:device:jar",
+    "io.ably.pubsub:server:jar",
+    "io.ably.pubsub:liveobjects:jar",
+    "io.ably.pubsub:pubsub-adapter:jar",
+    "io.ably.pubsub:network-client-core:jar",
+    "io.ably.pubsub:network-client-default:jar",
+    "io.ably.pubsub:network-client-okhttp:jar",
+)
+
+tasks.register("verifyReleaseArtifacts") {
+    description = "Asserts the published artifact set, group and lockstep version before a release."
+    doLast {
+        val rootVersion = project.property("VERSION_NAME") as String
+        val actual = sortedSetOf<String>()
+        subprojects.filter { it.pluginManager.hasPlugin("com.vanniktech.maven.publish") }.forEach { p ->
+            val artifactId = p.findProperty("POM_ARTIFACT_ID")
+                ?: error("${p.path} applies maven-publish but has no POM_ARTIFACT_ID")
+            val packaging = p.findProperty("POM_PACKAGING") ?: "jar"
+            // The version each module publishes at comes from its effective VERSION_NAME
+            // (a module-local gradle.properties can override the root's — exactly the
+            // lockstep drift this guards against).
+            val moduleVersion = p.findProperty("VERSION_NAME")
+            if (moduleVersion != rootVersion) {
+                error("Lockstep violation: ${p.path} has VERSION_NAME $moduleVersion, expected $rootVersion")
+            }
+            val group = p.findProperty("GROUP")
+            actual.add("$group:$artifactId:$packaging")
+        }
+        if (actual != expectedReleaseArtifacts) {
+            error(
+                "Published artifact set does not match the expected release set.\n" +
+                    "  expected: $expectedReleaseArtifacts\n" +
+                    "  actual:   $actual\n" +
+                    "If this change is deliberate, update expectedReleaseArtifacts in build.gradle.kts."
+            )
+        }
+        logger.lifecycle("Release pre-flight OK: ${actual.size} artifacts at $rootVersion: $actual")
+    }
+}
+
 configure(subprojects) {
     pluginManager.withPlugin("com.vanniktech.maven.publish") {
         extensions.configure<MavenPublishBaseExtension> {