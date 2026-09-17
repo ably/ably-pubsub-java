ably/ably-java

Run the UTS through the server door's builders
Mirrors ably-js#2294: the UTS constructs every client through a single
seam (TestRealtimeClient/TestRestClient in ClientFactories.kt), now
selected by the uts.side system property — `core` (default) keeps the
core constructors; `server` routes both client kinds through
PubSubServer's side-stamping builders. The builders only stamp the
side-declaring agent flag and pass everything else through (DebugOptions
included, via its copy() override), so conformance must be identical on
both legs; CI runs both for the UTS unit and integration tiers.

A harness self-test (SideModesTest, mirroring ably-js's
side_modes.test.ts) asserts each mode's stamp on the wire via the mock
HTTP engine — bare versionless flag in server mode, none in core mode —
so a broken seam cannot silently degrade the server leg into a duplicate
core run.

Unlike ably-js there is no device leg: io.ably.pubsub:device is an
Android artifact and cannot run on the JVM this suite uses; its stamping
contract is covered by the device module's instrumentation tests.

Also completes DebugOptions.copy() with the same four fields the base
ClientOptions.copy() was missing (headers, fallbackHosts,
transportParams, agents) — the door stamping relies on the polymorphic
copy() to carry the suite's mock hooks.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

uts/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 
 dependencies {
     testImplementation(project(":core"))
+    // The server door package, so the suite can run through its side-stamping builders
+    // (`-Duts.side=server`) as well as the core constructors. See ClientFactories.kt.
+    testImplementation(project(":server"))
     testImplementation(project(":network-client-core"))
     // Runtime-only so compile-time stays decoupled from the plugin internals; the LiveObjects test
     // helpers reach the internal wire/message classes (e.g. for build_public_object_message) by reflection.
@@ -38,6 +41,16 @@ tasks.withType<Test>().configureEach {
         providers.systemProperty("uts.proxy.localPath")
             .orElse(providers.environmentVariable("UTS_PROXY_LOCAL_PATH"))
             .getOrElse(""),
+    )
+
+    // Which package's entry points the suite constructs clients through: `core` (default) or
+    // `server` (the io.ably.pubsub:server builders). Forwarded explicitly for the same reason
+    // as uts.proxy.localPath above. See ClientFactories.kt.
+    systemProperty(
+        "uts.side",
+        providers.systemProperty("uts.side")
+            .orElse(providers.environmentVariable("UTS_SIDE"))
+            .getOrElse("core"),
     )
 }
 

ably/ably-pubsub-java

uts/build.gradle.kts
@@ -14,6 +14,9 @@ java {
 dependencies {
     // `api` for types that appear in infra signatures; `implementation` for internals.
     api(project(":core"))
+    // The server door package, so the suite can run through its side-stamping builders
+    // (`-Duts.side=server`) as well as the core constructors. See ClientFactories.kt.
+    implementation(project(":server"))
     api(project(":network-client-core"))
     // ktor stays implementation — the proxy infra uses it internally; it must NOT leak to consumers.
     implementation(libs.ktor.client.core)
@@ -50,6 +53,16 @@ tasks.withType<Test>().configureEach {
         providers.systemProperty("uts.proxy.localPath")
             .orElse(providers.environmentVariable("UTS_PROXY_LOCAL_PATH"))
             .getOrElse(""),
+    )
+
+    // Which package's entry points the suite constructs clients through: `core` (default) or
+    // `server` (the io.ably.pubsub:server builders). Forwarded explicitly for the same reason
+    // as uts.proxy.localPath above. See ClientFactories.kt.
+    systemProperty(
+        "uts.side",
+        providers.systemProperty("uts.side")
+            .orElse(providers.environmentVariable("UTS_SIDE"))
+            .getOrElse("core"),
     )
 }
 