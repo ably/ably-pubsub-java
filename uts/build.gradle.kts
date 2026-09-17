ably/ably-java

Restructure modules for the PubSub package split: java/android -> core/core-android
Applies the first step of PDR-091b to this repo: the shared implementation
becomes the new internal core artifacts, published under the io.ably.pubsub
group at the new 2.0.0 major.

- The `java` module becomes `core`, publishing io.ably.pubsub:core (jar).
- The `android` module becomes `core-android`, publishing
  io.ably.pubsub:core-android (aar).
- GROUP moves to io.ably.pubsub and VERSION_NAME to 2.0.0 for everything
  this repo publishes (liveobjects, pubsub-adapter and network-client-*
  move group alongside, per the rollout plan).
- Both core POMs state explicitly that they are internal implementation
  artifacts for Ably's own device/server packages, not for direct use.
- liveobjects, pubsub-adapter, uts and examples retarget their project
  dependencies; CI workflows, CONTRIBUTING and the uts README follow the
  new task paths.

The io.ably:ably-java and io.ably:ably-android artifacts are never
published from this branch again; 1.x fixes ship from a maintenance
branch per PDR-091b.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

uts/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    testImplementation(project(":java"))
+    testImplementation(project(":core"))
     testImplementation(project(":network-client-core"))
     // Runtime-only so compile-time stays decoupled from the plugin internals; the LiveObjects test
     // helpers reach the internal wire/message classes (e.g. for build_public_object_message) by reflection.

ably/ably-pubsub-java

uts/build.gradle.kts
@@ -13,7 +13,7 @@ java {
 
 dependencies {
     // `api` for types that appear in infra signatures; `implementation` for internals.
-    api(project(":java"))
+    api(project(":core"))
     api(project(":network-client-core"))
     // ktor stays implementation — the proxy infra uses it internally; it must NOT leak to consumers.
     implementation(libs.ktor.client.core)